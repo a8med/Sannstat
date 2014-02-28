c57bca08bcad11983552324ee1ca16ca6d76844d
Casper Renman <repsaccc@gmail.com>
2014-02-28 10:38:53 +0100

    Preparation nr 8 done.

--- lab2/src/f8.m
+++ lab2/src/f8.m
@@ -1,5 +1,18 @@
 % w = log(yk) = B0 + B1xk + ek 
-x = 5:50;
+x = 0.1:0.01:5;
 w = log(x');
-p = [ones(size(x)) x];
-[beta, beta2, residual] = regress(w, p)
+coeff = [ones(length(x'), 1) x'];
+[beta, beta2, residual] = regress(w, coeff);
+W = beta(1) + beta(2)*x';
+
+subplot(141);
+plot(x, log(x), 'r');
+
+subplot(142);
+plot(x', W, 'b');
+
+subplot(143);
+plot(x', residual, 'g');
+
+subplot(144);
+plot(x, log(x), 'r', x', W, 'b', x', residual, 'g');
