8d387628ab1fc07e4c84cc53b9d83a63745d1659
Buster Styren <styren@kth.se>
2014-02-28 13:49:08 +0100

    lab2: done.

--- lab2/src/p4a.m
+++ lab2/src/p4a.m
@@ -5,4 +5,6 @@ w = log(y);
 X = [ones(length(xtime), 1) xtime];
 [beta, betaint, res] = regress(w, X);
 
-beta(1) + beta(2)*2020
+syms x
+f = @(x)(beta(1) + beta(2)*x)
+f(2020)
--- lab2/src/p5.m
+++ lab2/src/p5.m
@@ -2,8 +2,20 @@ clear
 clf
 load birth.dat
 
+M = 1000;
+
 x = birth(birth(:, 20) < 3, 3);
 y = birth(birth(:, 20) == 3, 3);
 
-M = 100
-thetaboort = bootstrp(M, @mean, x) - bootstrp(M, @mean, y)
+ 
+mean(x) - mean(y)
+
+thetaboot = bootstrp(M, @mean, x) - bootstrp(M, @mean, y);
+
+subplot(121);
+normplot(thetaboot);
+
+quantile(thetaboot, [0.025, 0.975])
+
+d = sqrt(1/(M-1)*sum((thetaboot - mean(thetaboot)).^2));
+I = [mean(x) - mean(y) - 1.96*d, mean(x) - mean(y) + 1.96*d]
